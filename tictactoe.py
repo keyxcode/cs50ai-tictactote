d9715b801886660547325c905a447947fd426c01
keyxcode <keyxcode@gmail.com>
2023-07-19 18:18:36 +0700

    Implement terminal func

--- tictactoe.py
+++ tictactoe.py
@@ -81,7 +81,24 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    return True
+    won_rows = (
+        (board[0][0] == board[0][1] == board[0][2])
+        or (board[1][0] == board[1][1] == board[1][2])
+        or (board[2][0] == board[2][1] == board[2][2])
+    )
+    print("rows", won_rows)
+    won_cols = (
+        (board[0][0] == board[1][0] == board[2][0])
+        or (board[0][1] == board[1][1] == board[2][1])
+        or (board[0][2] == board[1][2] == board[2][2])
+    )
+    print("cols", won_cols)
+    won_diags = (board[0][0] == board[1][1] == board[2][2]) or (
+        board[0][2] == board[1][1] == board[2][0]
+    )
+    print("diags", won_diags)
+
+    return won_rows or won_cols or won_diags
 
 
 def utility(board):
